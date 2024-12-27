b149740ea933a559a8f3e62af3a7c9656454f420
Kuba Suder <jakub.suder@gmail.com>
2024-12-28 01:40:21 +0200

    added pds_host/labeler_host helpers that return just hostname

--- lib/didkit/services.rb
+++ lib/didkit/services.rb
@@ -11,5 +11,13 @@ module DIDKit
     def labeler_endpoint
       @labeler_endpoint ||= get_service('atproto_labeler', 'AtprotoLabeler')&.endpoint
     end
+
+    def pds_host
+      pds_endpoint&.then { |x| URI(x).host }
+    end
+
+    def labeler_host
+      labeler_endpoint&.then { |x| URI(x).host }
+    end
   end
 end
