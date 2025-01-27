828c8d5fdf16b39972d35c0820bbe59431138568
Kuba Suder <jakub.suder@gmail.com>
2025-01-27 23:43:12 +0200

    make (empty) tests pass

--- spec/didkit_spec.rb
+++ spec/didkit_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe Didkit do
+RSpec.describe DIDKit do
   it "has a version number" do
-    expect(Didkit::VERSION).not_to be nil
+    expect(DIDKit::VERSION).not_to be nil
   end
 end
