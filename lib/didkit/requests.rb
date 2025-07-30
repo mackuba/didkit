95a781434e55faa57cb78056adb1e6a8f55b938f
Kuba Suder <jakub.suder@gmail.com>
2025-07-30 22:59:31 +0200

    detect redirect loops

--- lib/didkit/requests.rb
+++ lib/didkit/requests.rb
@@ -11,9 +11,12 @@ module DIDKit::Requests
     }
 
     redirects = 0
+    visited_urls = []
     max_redirects = options[:max_redirects] || 5
 
     loop do
+      visited_urls << url
+
       response = Net::HTTP.start(url.host, url.port, request_options) do |http|
         request = Net::HTTP::Get.new(url)
         http.request(request)
@@ -21,7 +24,12 @@ module DIDKit::Requests
 
       if response.is_a?(Net::HTTPRedirection) && redirects < max_redirects && (location = response['Location'])
         url = URI(location.include?('://') ? location : (url.origin + location))
-        redirects += 1
+
+        if visited_urls.include?(url)
+          return response
+        else
+          redirects += 1
+        end
       else
         return response
       end
